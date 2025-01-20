6a9e41095a63d23e32910c19f928013560c9cc9a
lee juyoung <juyung0903@naver.com>
2025-01-20 12:16:57 +0900

    refactor: 메인페이지의 타이틀과 서브타이틀을 div에서 h1,h2태그로 변경

--- src/app/_component/home/HomeBackground.tsx
+++ src/app/_component/home/HomeBackground.tsx
@@ -7,10 +7,10 @@ export function HomeBackground() {
     <div className="flex w-full flex-col items-center gap-6 bg-home bg-cover bg-no-repeat px-5 py-20 text-sm leading-[19.6px]">
     {/* 타이틀과 서브타이틀 한 번 그룹핑 */}
     <div className="w-full flex-col flex gap-1 text-center">
-      <div className="text-2xl leading-[33.6px]">
+      <h1 className="text-2xl leading-[33.6px]">
         공연 <span className="font-semibold text-primary">같이 볼 프렌즈</span>가 필요한가요?
-      </div>
-      <div className="text-textLess">공연을 위한 나만의 페스트프렌즈를 찾아보세요!</div>
+      </h1>
+      <h2 className="text-textLess">공연을 위한 나만의 페스트프렌즈를 찾아보세요!</h2>
     </div>
   
     {/* 버튼 DIV 상위에서 gap-6을 통해 제목과 버튼 두 콘텐츠 간격을 조정 */}
