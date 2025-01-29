489a31ca9d652341944da9f0b8bac5ab9f8b70ab
lee juyoung <juyung0903@naver.com>
2025-01-29 18:50:51 +0900

    fix: Input을 React.forwardRef 및 input element의 attributes를 물려받을 수 있도록 수정

--- src/app/_component/ui/Input.tsx
+++ src/app/_component/ui/Input.tsx
@@ -1,73 +1,83 @@
 'use client';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import React from 'react';
 
-interface InputProps {
-  type?: string;
-  placeholder: string;
-  value?: string;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   role?: 'search' | 'password';
-  isImg?: boolean; // Input에 이미지 추가 여부 ex)password, search
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isImg?: boolean; // Input에 이미지 추가 여부 ex) password, search
 }
 
-export default function Input({
-  type = 'text',
-  placeholder,
-  value,
-  role,
-  onChange,
-  isImg = false,
-}: InputProps) {
-  const [isClient, setIsclient] = useState(false);
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      type = 'text',
+      placeholder,
+      value,
+      onChange,
+      role,
+      isImg = false,
+      ...props
+    },
+    ref
+  ) => {
+    const [isClient, setIsClient] = useState(false);
 
-  // Input Focus Border Control
-  const [isFocused, setIsFocused] = useState(false);
-  const handleFocus = () => setIsFocused(true);
-  const handleBlur = () => setIsFocused(false);
+    // Input Focus Border Control
+    const [isFocused, setIsFocused] = useState(false);
+    const handleFocus = () => setIsFocused(true);
+    const handleBlur = () => setIsFocused(false);
 
-  // Show Password Control
-  const [inputType, setInputType] = useState(type);
-  const togglePWVisibility = () => {
-    setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
-  };
+    // Show Password Control
+    const [inputType, setInputType] = useState(type);
+    const togglePWVisibility = () => {
+      setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
+    };
 
-  useEffect(() => {
-    setIsclient(true);
-  }, []);
+    useEffect(() => {
+      setIsClient(true);
+    }, []);
 
-  return (
-    isClient && (
-      <div
-        className={`flex w-full items-center gap-3 rounded-xl border bg-areaBg px-4 py-3 ${isFocused ? 'border-primary' : 'border-transparent'}`}
-      >
-        <input
-          onChange={onChange}
-          type={inputType}
-          placeholder={placeholder}
-          value={value}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          className="placeholder:placeholder w-full bg-transparent text-sm text-white focus:outline-none"
-        />
+    return (
+      isClient && (
+        <div
+          className={`flex w-full items-center gap-3 rounded-xl border bg-areaBg px-4 py-3 ${
+            isFocused ? 'border-primary' : 'border-transparent'
+          }`}
+        >
+          <input
+            ref={ref} // React Hook Form과 호환되도록 ref 전달
+            onChange={onChange}
+            type={inputType}
+            placeholder={placeholder}
+            value={value}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            className="placeholder:placeholder w-full bg-transparent text-sm text-white focus:outline-none"
+            {...props} // 기타 전달된 props
+          />
 
-        {isImg && (
-          <Image
-            width={20}
-            height={20}
-            src={
-              role === 'search'
-                ? '/image/search.png'
-                : inputType === 'password'
+          {isImg && (
+            <Image
+              width={20}
+              height={20}
+              src={
+                role === 'search'
+                  ? '/image/search.png'
+                  : inputType === 'password'
                   ? '/image/visibility_off.png'
                   : '/image/visibility.png'
-            }
-            alt="toggle visibility"
-            onClick={role === 'password' ? togglePWVisibility : undefined}
-            className={role === 'password' ? 'cursor-pointer' : ''}
-          />
-        )}
-      </div>
-    )
-  );
-}
+              }
+              alt="toggle visibility"
+              onClick={role === 'password' ? togglePWVisibility : undefined}
+              className={role === 'password' ? 'cursor-pointer' : ''}
+            />
+          )}
+        </div>
+      )
+    );
+  }
+);
+
+Input.displayName = 'Input'; // React.forwardRef 사용 시 컴포넌트 이름 지정
+export default Input;
