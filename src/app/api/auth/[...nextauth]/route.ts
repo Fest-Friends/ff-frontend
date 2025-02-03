76906143f4555393d689af5333e4b8c9c9e2f034
lee juyoung <juyung0903@naver.com>
2025-02-03 11:38:13 +0900

    setting: next-auth삭제

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
-
-// GET(or POST) /api/auth/a
-// GET(or POST) /api/auth/b
-// GET(or) /api/auth/c/a 다 가능
-
-
-
