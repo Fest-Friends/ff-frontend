bfd9609f7a64aef8b6642f6ced25d6726cc0415d
Kim HeeChan <h22chan.dev@gmail.com>
2025-02-21 00:26:41 +0900

    feat: isRead CSS 추가

--- src/app/(afterLogin)/notification/_component/NoticeList.tsx
+++ src/app/(afterLogin)/notification/_component/NoticeList.tsx
@@ -5,7 +5,7 @@ export default function NoticeList() {
     <div className="flex flex-col">
       {/* 알림 리스트 API 이후 map으로 NoticeItem 뿌리기 */}
       <NoticeItem time="6시간 전" user="김희찬님" />
-      <NoticeItem time="1일 전" user="쏜애플님"/>
+      <NoticeItem time="1일 전" user="쏜애플님" isRead />
     </div>
   );
 }
