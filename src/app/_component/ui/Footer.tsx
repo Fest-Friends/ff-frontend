9c30966fe3c22d2a3162235a3ff53d313d7c0afa
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2025-02-28 16:25:04 +0900

    feat : footer 조건에따라 보이게

--- src/app/_component/ui/Footer.tsx
+++ src/app/_component/ui/Footer.tsx
@@ -1,10 +1,19 @@
+'use client';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Footer() {
+  const pathname = usePathname();
+  const hideFooterPages = ['/login', '/signUp'];
+
+  if (hideFooterPages.includes(pathname)) {
+    return null;
+  }
+
   return (
-    <div className='px-5 py-6 flex flex-col gap-6 bg-areaBg'>
-      <div className='flex items-center justify-between'>
+    <div className="flex flex-col gap-6 bg-areaBg px-5 py-6">
+      <div className="flex items-center justify-between">
         <Image width={118} height={31} alt="푸터 로고" src="/image/logo.png" />
         <Link href="https://github.com/Fest-Friends">
           <Image
@@ -18,29 +27,29 @@ export default function Footer() {
       </div>
 
       {/* 푸터 정보 */}
-      <div className='flex flex-col gap-1'>
-        <div className='flex text-sm text-textLess'>
-          <p className='w-[60px]'>Project</p>
-          <p className='mr-2'>ㅣ</p>
+      <div className="flex flex-col gap-1">
+        <div className="flex text-sm text-textLess">
+          <p className="w-[60px]">Project</p>
+          <p className="mr-2">ㅣ</p>
           <p>GDGoc TUK 3기 Project</p>
         </div>
 
-        <div className='flex text-sm text-textLess'>
-          <p className='w-[60px]'>Member</p>
-          <p className='mr-2'>ㅣ</p>
+        <div className="flex text-sm text-textLess">
+          <p className="w-[60px]">Member</p>
+          <p className="mr-2">ㅣ</p>
           <p>Luke, Jenna, Dan, Jun, Ann</p>
         </div>
 
-        <div className='flex text-sm text-textLess'>
-          <p className='w-[60px]'>Period</p>
-          <p className='mr-2'>ㅣ</p>
+        <div className="flex text-sm text-textLess">
+          <p className="w-[60px]">Period</p>
+          <p className="mr-2">ㅣ</p>
           <p>24.11.01 - 25.03.04</p>
         </div>
       </div>
 
       <div>
-        <hr className='h-[1px] border-none bg-textMuted mb-2' />
-        <p className='text-xs text-textMuted'>Copyright © FestFriends All Rights Reserved.</p>
+        <hr className="mb-2 h-[1px] border-none bg-textMuted" />
+        <p className="text-xs text-textMuted">Copyright © FestFriends All Rights Reserved.</p>
       </div>
     </div>
   );
